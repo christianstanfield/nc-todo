33fe2214dfa48b3253131aaab4c955da2f32385b
Christian Stanfield <Ein@MacBook-Pro.local>
2014-11-28 11:08:53 -0600

    began basic login functionality

---
+++ app/assets/javascripts/sessions.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+$('#login_button').on('click', function(e) {
+  e.preventDefault(); // needs default functionality
+  $('#login_button').toggleClass('clicked');
+  $('#login_menu').toggle();
+});
+
+
+});
